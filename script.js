d1237c82e65782fc41775d51fd227dc0b979d0f5
$econd <154570179+SecondRoblox@users.noreply.github.com>
2024-10-11 08:51:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const { exec } = require('child_process');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('command', (cmd) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                socket.emit('output', `Error: ${stderr}`);
+            } else {
+                socket.emit('output', stdout);
+            }
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
 
